0febf5d95bad019fc0a06fb66f8e26f53ce36dd2
ygorjg <ygorjgr@gmail.com>
2023-05-17 17:34:06 -0300

    feat: update cors configs and remove unused itens

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { AppDataSource } from "./data-source";
 import routes from "./routes";
 
 AppDataSource.initialize().then(() => {
   const app = express();
+
+  app.use(cors());
 
   app.use(express.json());
 
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const App = () => {
-  return <h1>Hello World!</h1>;
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/companies").then((response) => {
+      setCompanies(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Empresas cadastradas</h1>
+      <ul>
+        {companies.map((companie) => {
+          return (
+            <li key={companie.id}>
+              <h2>{companie.name}</h2>
+              <p>{companie.city}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
